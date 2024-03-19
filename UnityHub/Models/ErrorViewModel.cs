426a25227144f79ae541fa7a0af6e03188691017
Dianaleite01 <aluno23392@ipt.pt>
2024-03-19 09:13:16 +0000

    Add project files.

---
+++ UnityHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnityHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
