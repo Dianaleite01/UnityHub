806bab322b1ab7aedde792736bf5c8ad147691d2
Dianaleite01 <aluno23392@ipt.pt>
2024-06-27 23:27:24 +0100

    base APIs
    apenas a base para efetuarmos a nossa API

---
+++ UnityHub/Controllers/CandidaturasAPI.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UnityHub.Data;
+using UnityHub.Models;
+using System.IO;
+
+namespace UnityHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidaturasAPI : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CandidaturasAPI(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Candidaturas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Candidaturas>>> GetCandidaturas()
+        {
+
+            return await _context.Candidaturas.ToListAsync();
+        }
+
+        // GET: api/Candidaturas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidaturas>> GetCandidaturas(int id)
+        {
+            var candidatura = await _context.Candidaturas.FindAsync(id);
+
+            if (candidatura == null)
+            {
+                return NotFound();
+            }
+
+            return candidatura;
+
+        }
+    }
+}
---
+++ UnityHub/Controllers/CategoriasAPI.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UnityHub.Data;
+using UnityHub.Models;
+using System.IO;
+
+namespace UnityHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasAPI : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasAPI(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categorias>>> GetCategorias()
+        {
+
+            return await _context.Categorias.ToListAsync();
+        }
+
+        // GET: api/Categorias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categorias>> GetCategorias(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+
+        }
+    }
+}
