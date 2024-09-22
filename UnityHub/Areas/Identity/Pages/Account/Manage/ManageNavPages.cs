a5a3bedb2a7b1670ad983c96f339a9017a0aefb1
Dianaleite01 <aluno23392@ipt.pt>
2024-09-22 02:17:49 +0100

    Alteração da password
    Adicionamos um "manage" de alterar a password, de momento é possível alterar a password mas não coneguimos fazer uma rota correta

---
+++ UnityHub/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace  UnityHub.Areas.Identity.Pages.Account.Manage
+{
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public static class ManageNavPages
+    {
+       
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public static string ChangePassword => "ChangePassword";
+
+      
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+
+      
+        
+        public static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
